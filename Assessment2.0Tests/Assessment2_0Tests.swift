e173eafe4e6318ebeab7f98200e331142236b019
Kayn Critchley <kayncritchley@kayns-mac.local>
2019-04-15 21:45:26 -0700

    Initial Commit

---
+++ Assessment2.0Tests/Assessment2_0Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Assessment2_0Tests.swift
+//  Assessment2.0Tests
+//
+//  Created by Kayn Critchley on 15/4/19.
+//  Copyright © 2019 Kayn Critchley. All rights reserved.
+//
+
+import XCTest
+@testable import Assessment2_0
+
+class Assessment2_0Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
